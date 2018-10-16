c26e38ef65f1653057673aa29eddea09a959d2b8
Sua <rkttndk@naver.com>
2018-10-16 16:56:44 +0900

    added turbo function

--- myapp.c
+++ myapp.c
@@ -3,12 +3,22 @@
 
 #include <stdio.h> 
 
+void turbo_on(){
+printf("turbo on");
+printf("\n");
+}
 
+void turbo_off(){
+printf("turbo off\n");
+}
 
 int main(){
 
 func1();
 func2();
+
+turbo_on();
+turbo_off();
 
 return 0;
 }
